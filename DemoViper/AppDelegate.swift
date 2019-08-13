2bbae33941fae3cf37f4e8aaf1dd428e78b90303
Issarapong Poesua <dekablade01@gmail.com>
2019-08-13 12:11:28 +0700

    set view controller from app delegate

--- DemoViper/AppDelegate.swift
+++ DemoViper/AppDelegate.swift
@@ -13,9 +13,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        window = window ?? UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
         return true
     }
 
