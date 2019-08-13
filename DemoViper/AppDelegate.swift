a33868da7a223f10f42585899d4d83a97a6e8e26
Issarapong Poesua <dekablade01@gmail.com>
2019-08-13 12:12:03 +0700

    add comment.

--- DemoViper/AppDelegate.swift
+++ DemoViper/AppDelegate.swift
@@ -15,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = window ?? UIWindow(frame: UIScreen.main.bounds)
+        // note: configure application module here. 
         window?.rootViewController = ViewController()
         window?.makeKeyAndVisible()
         return true
