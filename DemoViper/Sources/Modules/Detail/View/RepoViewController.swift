b27abccc863695b34a2057b1b0f98e7395326fa7
Su Ho <su@nimblehq.co>
2019-09-12 10:04:29 +0700

    Remove xib

--- DemoViper/Sources/Modules/Detail/View/RepoViewController.swift
+++ DemoViper/Sources/Modules/Detail/View/RepoViewController.swift
@@ -11,10 +11,13 @@ import UIKit
 final class RepoViewController: UIViewController {
 
     // MARK: - Outlets
-    @IBOutlet weak var repoNameLabel: UILabel!
-    @IBOutlet weak var descriptionLabel: UILabel!
-    @IBOutlet weak var avatarImageView: UIImageView!
-    @IBOutlet weak var authorNameLabel: UILabel!
+    let repoNameLabel = UILabel()
+    let descriptionLabel = UILabel()
+
+    let authorViewTitle = UILabel()
+    let authorView = UIView()
+    let avatarImageView = UIImageView()
+    let authorNameLabel = UILabel()
     
     var output: RepoViewOutput?
 
@@ -34,6 +37,7 @@ extension RepoViewController: RepoViewInput {
         navigationItem.title = item.repoFullName
         repoNameLabel.text = item.repoName
         descriptionLabel.text = item.description
+        authorViewTitle.text = "@author"
         authorNameLabel.text = item.authorName
     }
 
@@ -45,10 +49,61 @@ extension RepoViewController: RepoViewInput {
 // MARK: - UI
 extension RepoViewController {
     private func setupUI() {
-        setupAvatar()
+        setUpLayouts()
+        setUpViews()
     }
 
-    private func setupAvatar() {
+    private func setUpLayouts() {
+        view.addSubview(repoNameLabel)
+        view.addSubview(descriptionLabel)
+        view.addSubview(authorViewTitle)
+        view.addSubview(authorView)
+        authorView.addSubview(avatarImageView)
+        authorView.addSubview(authorNameLabel)
+
+        repoNameLabel.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(view.layoutMarginsGuide).inset(20.0)
+        }
+
+        descriptionLabel.snp.makeConstraints {
+            $0.top.equalTo(repoNameLabel.snp.bottom).inset(-20.0)
+            $0.left.right.equalToSuperview().inset(8.0)
+        }
+
+        authorViewTitle.snp.makeConstraints {
+            $0.top.equalTo(descriptionLabel.snp.bottom).inset(-20.0)
+            $0.left.equalToSuperview().inset(8.0)
+        }
+
+        authorView.snp.makeConstraints {
+            $0.left.right.equalToSuperview().inset(8.0)
+            $0.top.equalTo(authorViewTitle.snp.bottom).inset(-20.0)
+            $0.height.equalTo(80.0)
+        }
+
+        avatarImageView.snp.makeConstraints {
+            $0.top.left.bottom.equalToSuperview()
+            $0.width.equalTo(avatarImageView.snp.height)
+        }
+
+        authorNameLabel.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.right.equalToSuperview().inset(8.0)
+            $0.left.equalTo(avatarImageView.snp.right).inset(-8.0)
+        }
+    }
+
+    private func setUpViews() {
+        view.backgroundColor = .white
+        repoNameLabel.font = .boldSystemFont(ofSize: 19.0)
+        descriptionLabel.numberOfLines = 0
+
+        authorViewTitle.font = .boldSystemFont(ofSize: 17.0)
+
+        let tap = UITapGestureRecognizer(target: self, action: #selector(didTapOwner))
+        authorView.addGestureRecognizer(tap)
+
         avatarImageView.layer.cornerRadius = avatarImageView.bounds.height / 2
         avatarImageView.layer.masksToBounds = true
     }
@@ -56,7 +111,7 @@ extension RepoViewController {
 
 // MARK: - Actions
 extension RepoViewController {
-    @IBAction func didTapOwner(_ sender: Any) {
+    @objc private func didTapOwner(_ sender: Any) {
         output?.openWebPage()
     }
 }
